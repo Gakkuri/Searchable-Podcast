a407a17b6dca4e282cbd7402ab7100fbc4254049
Lance Samonte <samonte.lance.22@gmail.com>
2024-01-31 00:17:06 +0800

    remove link in accessing api

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 import axios from "axios";
 
 export default async function Home() {
-  const { data } = await axios.get("http://localhost:3000/api/download");
-  console.log(data);
+  const { data } = await axios.get("api/download");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
